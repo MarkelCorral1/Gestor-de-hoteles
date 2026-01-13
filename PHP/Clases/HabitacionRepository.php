1a7cf940345460c57393165bc9e5724da02e6146
MarkelCorral1 <24dw.markel.corral@arangoya.net>
2026-01-13 22:35:53 +0100

    Crear metodo que devuelva habitaciones de un hotel -markel

--- PHP/Clases/HabitacionRepository.php
+++ PHP/Clases/HabitacionRepository.php
@@ -2,4 +2,10 @@
 use Doctrine\ORM\EntityRepository;
 
 class HabitacionRepository extends EntityRepository{
+    public function habitacionPorHotel($id_hotel) {
+        return $this->getEntityManager()
+            ->createQuery('SELECT h FROM Habitacion h WHERE h.id_hotel = :id_hotel')
+            ->setParameter('id_hotel', $id_hotel)
+            ->getResult();
+    }
 }
