f696d9f3e87a5ada589a36b6519e3b2541470f6c
develop <24dw.alejandro.rabasot@arangoya.net>
2026-01-21 13:35:08 +0100

    Añadido bootstrap - Alejandro

--- webpages/listaHabitaciones.php
+++ webpages/listaHabitaciones.php
@@ -87,7 +87,7 @@ require_once '../config/config.php';
     </div>
 
     <?php include INCLUDES_PATH . '/footer.php'; ?>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
