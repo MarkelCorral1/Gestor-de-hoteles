2bde7288e7312e8ed851d9b5a509534ac4adb0a0
Markel <24dw.markel.corral@arangoya.net>
2026-01-22 14:21:41 +0100

    Añadir lista de iconos a las categorias -Markel

--- JS/datosCategorias.js
+++ JS/datosCategorias.js
@@ -1,3 +1,12 @@
+// nombres bootstrap de los iconos de servicios (balcon, yakushi, spa, mayordomo, limusina, helicoptero)
+iconosServicios = {
+    balcon: 'bi-building',
+    yakushi: 'bi-droplet-half',
+    spa: 'bi-flower2',
+    mayordomo: 'bi-person-bounding-box',
+    limusina: 'bi-car-front',
+    helicoptero: 'bi-airplane'
+}
 document.addEventListener('DOMContentLoaded', () => {
     const categorias = document.querySelectorAll('.categorie-item');
 
@@ -34,9 +43,27 @@ function fetchCategoria(catNombre) {
                 document.getElementById('cat-metros').innerText = data.metros;
                 document.getElementById('cat-camas').innerText = data.camas;
 
+
+                let servicios  = document.getElementById('servicios-lista');
+                // Wifi, Parking, Desayuno, Piscina, Gimnasio, Cine, Sala conferencias, Sala juegos, casino incluidos
+                servicios.innerHTML = '<div class="servicio-item servicio-item d-flex align-items-center bg-danger bg-gradient rounded p-1"><span>Wifi</span><i class="ms-2 fs-4 bi bi-wifi"></i></div>' +
+                                     '<div class="servicio-item servicio-item d-flex align-items-center bg-danger bg-gradient rounded p-1"><span>Parking</span><i class="ms-2 fs-4 bi bi-p-square"></i></div>' +
+                                     '<div class="servicio-item servicio-item d-flex align-items-center bg-danger bg-gradient rounded p-1"><span>Desayuno</span><i class="ms-2 fs-4 bi bi-cup-straw"></i></div>' +
+                                     '<div class="servicio-item servicio-item d-flex align-items-center bg-danger bg-gradient rounded p-1"><span>Piscina</span><i class="ms-2 fs-4 bi bi-water"></i></div>' +
+                                     '<div class="servicio-item servicio-item d-flex align-items-center bg-danger bg-gradient rounded p-1"><span>Gimnasio</span><i class="ms-2 fs-4 bi bi-lightning-charge"></i></div>' +
+                                     '<div class="servicio-item servicio-item d-flex align-items-center bg-danger bg-gradient rounded p-1"><span>Cine</span><i class="ms-2 fs-4 bi bi-film"></i></div>' +
+                                     '<div class="servicio-item servicio-item d-flex align-items-center bg-danger bg-gradient rounded p-1"><span>Sala conferencias</span><i class="ms-2 fs-4 bi bi-easel"></i></div>' +
+                                     '<div class="servicio-item servicio-item d-flex align-items-center bg-danger bg-gradient rounded p-1"><span>Sala juegos</span><i class="ms-2 fs-4 bi bi-controller"></i></div>' +
+                                     '<div class="servicio-item servicio-item d-flex align-items-center bg-danger bg-gradient rounded p-1"><span>Casino</span><i class="ms-2 fs-4 bi bi-dice-6"></i></div>';
+
                 // Recorrer todos los servicios y sus valores
                 Object.entries(data.servicios).forEach(([nombre, tiene]) => {
                     console.log(`${nombre} : ${tiene}`);
+                    if (tiene) {
+                        // pasar a innerHTML
+                        servicios.innerHTML +=
+                            `<div class="servicio-item servicio-item d-flex align-items-center bg-danger bg-gradient rounded p-1"><span>${nombre}</span><i class="ms-2 fs-4 bi ${iconosServicios[nombre]}"></i></div>`;
+                    }
                 });
             } else {
                 console.error('Error:', data.message);
