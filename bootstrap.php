9b4f8df6b185076e94ead15bc6d0fd44d4819359
MarkelCorral1 <24dw.markel.corral@arangoya.net>
2026-01-26 20:39:54 +0100

    config.php y bootstrap.php ahora usan variables de entorno -Markel --- IMPORTANTE --- Ahora se usan variables de entorno, para que PHP funcione con ellas hay que ejecutar en la terminal: composer require vlucas/phpdotenv
    Despues habra que crear un archivo .env y rellenarle la informacion. Si se estaba ejecuanto en windows deberia quedar asi:
    DB_USER=root
    DB_PASSWORD=root
    DB_NAME=hoteles_schumacher
    DB_HOST=localhost
    BASE_URL=http://localhost//DAW/Gestor-de-hoteles

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,19 @@
 <?php
 require_once 'vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 $paths = array('./src');
 $isDevMode = true;
 $dbParams = array(
  'driver' => 'pdo_mysql',
- 'user' => 'root',
- 'password' => 'root',
- 'dbname' => 'hoteles_schumacher',
- 'host' => 'localhost',
+ 'user' => $_ENV['DB_USER'],
+ 'password' => $_ENV['DB_PASSWORD'],
+ 'dbname' => $_ENV['DB_NAME'],
+ 'host' => $_ENV['DB_HOST'],
 );
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, null, null, false);
 $entityManager = EntityManager::create($dbParams, $config);
--- config/config.php
+++ config/config.php
@@ -1,5 +1,10 @@
 <?php
-define('BASE_URL', 'http://localhost//DAW/Gestor-de-hoteles');
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+define('BASE_URL', $_ENV['BASE_URL']);
 define('IMAGES_URL', BASE_URL . '/images');
 define('CSS_URL', BASE_URL . '/scss/main.css'); 
 define('JS_URL', BASE_URL . '/JS');
