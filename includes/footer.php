9359f8810c80f051a66e9f1edeee9d2d13b23808
MarkelCorral1 <24dw.markel.corral@arangoya.net>
2026-02-02 16:30:39 +0100

    Cambiar footer -Markel

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,10 @@
 <footer id="footer" class="py-4">
     <div class="container text-center">
         <h5><img src="<?= IMAGES_URL ?>/Logo/Logo.png" alt="Logo" class="img-footer"></h5>
-        <p>Madrid · París · Dubái · Maldivas · Nueva York · Tokio · Zúrich</p>
+        <div class="my-4">
+                Madrid · Barcelona · París · Londres · Roma ...<br>
+                14 hoteles a lo largo de toda Europa
+        </div>
         <p> info@schumacherhotels.com | +34 912 345 678</p>
         <p class="small">© 2026 Schumacher – Colección Privada</p>
     </div>
